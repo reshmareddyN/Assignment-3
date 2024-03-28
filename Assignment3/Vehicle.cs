a33ab502da7546b30dba210b73fa8b7872c31544
Reshma_Nalamalapu <rn21aae@herts.ac.uk>
2024-03-27 20:51:42 -0400

    changed vehicle class

--- Assignment3/Vehicle.cs
+++ Assignment3/Vehicle.cs
@@ -1,12 +1,12 @@
 ﻿public class Vehicle
 {
-    // Properties
+   
     public string Model { get; set; }
     public string Manufacturer { get; set; }
     public int Year { get; set; }
     public decimal RentalPrice { get; set; }
 
-    // Constructor
+    
     public Vehicle(string model, string manufacturer, int year, decimal rentalPrice)
     {
         Model = model;
@@ -15,7 +15,7 @@
         RentalPrice = rentalPrice;
     }
 
-    // Method to display vehicle details
+    
     public virtual void DisplayDetails()
     {
         Console.WriteLine($"Model: {Model}, Manufacturer: {Manufacturer}, Year: {Year}, Rental Price: {RentalPrice:C}");
