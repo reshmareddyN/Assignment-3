c5d274807d5fc9a04656a60aaf4925fe219f20cb
Reshma_Nalamalapu <rn21aae@herts.ac.uk>
2024-03-27 20:55:30 -0400

    added rentalagency class

---
+++ Assignment3/RentalAgency.cs
@@ -0,0 +1,34 @@
+﻿public class RentalAgency
+{
+    // Properties
+    private List<Vehicle> Fleet { get; set; }
+    public decimal TotalRevenue { get; private set; }
+
+    
+    public RentalAgency()
+    {
+        Fleet = new List<Vehicle>();
+        TotalRevenue = 0;
+    }
+
+    
+    public void AddVehicle(Vehicle vehicle)
+    {
+        Fleet.Add(vehicle);
+    }
+
+    
+    public void RemoveVehicle(Vehicle vehicle)
+    {
+        Fleet.Remove(vehicle);
+    }
+
+    
+    public void RentVehicle(Vehicle vehicle, int days)
+    {
+        decimal rentalCost = vehicle.RentalPrice * days;
+        TotalRevenue += rentalCost;
+        Console.WriteLine($"Vehicle {vehicle.Model} rented for {days} days. Total cost: {rentalCost:C}");
+    }
+}
+
