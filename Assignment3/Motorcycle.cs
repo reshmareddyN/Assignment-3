579158a4ab52cc99ed1c4b7bab531b0426abbf51
Reshma_Nalamalapu <rn21aae@herts.ac.uk>
2024-03-27 20:53:41 -0400

    added motorcycle class

---
+++ Assignment3/Motorcycle.cs
@@ -0,0 +1,25 @@
+﻿public class Motorcycle : Vehicle
+{
+    
+    public int EngineCapacity { get; set; }
+    public string FuelType { get; set; }
+    public bool HasFairing { get; set; }
+
+    
+    public Motorcycle(string model, string manufacturer, int year, decimal rentalPrice,
+                      int engineCapacity, string fuelType, bool hasFairing)
+        : base(model, manufacturer, year, rentalPrice)
+    {
+        EngineCapacity = engineCapacity;
+        FuelType = fuelType;
+        HasFairing = hasFairing;
+    }
+
+    
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine($"Engine Capacity: {EngineCapacity}, Fuel Type: {FuelType}, Has Fairing: {HasFairing}");
+    }
+}
+
