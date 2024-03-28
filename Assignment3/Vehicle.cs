acc8f8c1a6452c50c6e420d819505ba8acd79a3b
Reshma_Nalamalapu <rn21aae@herts.ac.uk>
2024-03-27 20:31:29 -0400

    Added Vehicle class

--- Assignment3/Vehicle.cs
+++ Assignment3/Vehicle.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Assignment3
+﻿public class Vehicle
 {
-    internal class Vehicle
+    // Properties
+    public string Model { get; set; }
+    public string Manufacturer { get; set; }
+    public int Year { get; set; }
+    public decimal RentalPrice { get; set; }
+
+    // Constructor
+    public Vehicle(string model, string manufacturer, int year, decimal rentalPrice)
     {
+        Model = model;
+        Manufacturer = manufacturer;
+        Year = year;
+        RentalPrice = rentalPrice;
+    }
+
+    // Method to display vehicle details
+    public virtual void DisplayDetails()
+    {
+        Console.WriteLine($"Model: {Model}, Manufacturer: {Manufacturer}, Year: {Year}, Rental Price: {RentalPrice:C}");
     }
 }
